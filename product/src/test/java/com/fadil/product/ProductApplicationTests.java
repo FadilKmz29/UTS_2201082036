99155a8be5c444528da4d1a679acaa2d65e37ce7
FadilKmz29 <mfadilahkamza@gmail.com>
2024-05-20 08:29:59 +0700

    s

---
+++ product/src/test/java/com/fadil/product/ProductApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fadil.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
